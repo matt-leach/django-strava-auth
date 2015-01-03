8aca7b550ccd2fcda98785d4f292f947a7ed2d20
Matt <matt.leach25@gmail.com>
2015-01-03 15:29:12 +0000

    Adding Strava authenticate function

--- stravauth/backend.py
+++ stravauth/backend.py
@@ -1,21 +1,43 @@
 from django.conf import settings
+from django.contrib.auth.models import User
 
-
+from stravauth.client import StravaClient
+from stravauth.models import StravaToken 
 
 class StravaV3Backend(object):
     """
     Authenticate using the Strava V3 API.
     """
     def authenticate(self, code):
-        pass
-        # Make the request to the API to get the token
+        client_id = settings.CLIENT_ID
+        client_secret = settings.CLIENT_SECRET
         
+        # Make the request to the API
+        c = StravaClient()
+        response = c.get_token(client_id, client_secret, code)
         
-        # Get/Create the user
+        access_token = response["access_token"]
+        user_id = response["athlete"]["id"]
+        username = "%s %s" % (response["athlete"]["firstname"], response["athlete"]["lastname"])
         
+        # Get or create the user
+        try:
+            user = User.objects.get(id=user_id)
+        except User.DoesNotExist:
+            # Otherwise create the user
+            user = User(id=user_id, username=username)
+            user.save()  
         
-        # Create the token model
+        # Add the token 
+        try:
+            token_model = user.stravatoken
+        except StravaToken.DoesNotExist:
+            token_model = StravaToken(user=user)
+            
+        token_model.token = access_token
+        token_model.save()
         
+        # Return the user
+        return user
         
-        # return the user 
         
--- stravauth/client.py
+++ stravauth/client.py
@@ -1,3 +1,4 @@
+import requests
 
 
 class StravaClient(object):
@@ -5,3 +6,14 @@ class StravaClient(object):
     Object used to access Strava API
     """
     api_endpoint = "https://www.strava.com"
+    
+    
+    def get_token(self, client_id, client_secret, code):
+        data = {"client_id": client_id, "client_secret": client_secret, "code": code}
+        
+        r = requests.post("%s/oauth/token" % self.api_endpoint, data=data)
+        
+        # TODO: Error handling 
+        response = r.json()
+        
+        return response
